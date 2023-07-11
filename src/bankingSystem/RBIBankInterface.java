24d3ef4a1d1adf419a190bcc198c63844629b5df
Tushar <tushargarg0110@gmail.com>
2023-07-11 22:18:47 +0530

    Banking application

---
+++ src/bankingSystem/RBIBankInterface.java
@@ -0,0 +1,15 @@
+package bankingSystem;
+
+public interface RBIBankInterface {
+
+
+    public String depositMoney(int money);
+
+    public String withdrawMoney(int money,String password);
+
+    public String checkBalance(String password);
+
+    public Double calculateTotalInterest(int time);
+
+}
+
